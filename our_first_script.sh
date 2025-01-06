fa22370aeb1a76cfb1ea7b6ab2a37032f9c15974
robson01jc <robson01jc@gmail.com>
2025-01-06 08:53:52 -0300

    first commit

---
+++ our_first_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author: Robson Carvalho
+# Date Created: 06/01/2025
+# Last Modified: 06/01/2025
+
+# Description
+# Prints "This is my first script" to the terminal
+
+# Usage
+# our_first_script.sh
+
+echo "This is my first script"
+
+exit 0
